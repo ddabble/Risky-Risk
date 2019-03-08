0b2eabf252fe2d3322a5fd2c268f5e345e1ed8bb
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2019-03-08 14:12:34 +0100

    #1 #2 #3 Changes, new components added, removed some empty entities and components.

--- core/src/no/ntnu/idi/tdt4240/Components/TileNeutralTextureComponent.java
+++
@@ -1,6 +0,0 @@
-package no.ntnu.idi.tdt4240.Components;
-
-import com.badlogic.ashley.core.Component;
-
-public class TileNeutralTextureComponent implements Component {
-}
--- core/src/no/ntnu/idi/tdt4240/Entities/Player.java
+++
@@ -1,6 +0,0 @@
-package no.ntnu.idi.tdt4240.Entities;
-
-import com.badlogic.ashley.core.Entity;
-
-public class Player {
-}
