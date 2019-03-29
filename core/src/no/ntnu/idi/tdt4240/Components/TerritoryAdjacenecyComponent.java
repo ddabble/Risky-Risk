5ba9bbf11578b0ad006fd9c24e51dc325b4b7e33
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2019-03-29 12:27:24 +0100

    First draft of google matchmaking and initial code for Hashmap of adjacencystructure

---
+++ core/src/no/ntnu/idi/tdt4240/Components/TerritoryAdjacenecyComponent.java
@@ -0,0 +1,68 @@
+package no.ntnu.idi.tdt4240.Components;
+
+import com.badlogic.ashley.core.Component;
+
+import java.util.HashMap;
+
+public class TerritoryAdjacenecyComponent implements Component {
+
+    public HashMap<Integer, Integer[]> AdjacentCountries = new HashMap<Integer, Integer[]>();
+    AdjacentCountries.put()
+
+
+
+
+
+    put(0x6A8600, "North America 1");
+    put(0x3F5437, "North America 2");
+    put(0xFFFF00, "North America 3");
+    put(0x949449, "North America 4");
+    put(0xD1FF80, "North America 5");
+    put(0x505027, "North America 6");
+    put(0x808000, "North America 7");
+    put(0xFFFF80, "North America 8");
+    put(0xA27300, "North America 9");
+
+    // South America
+    put(0xFF8080, "South America 1");
+    put(0x800000, "South America 2");
+    put(0x804040, "South America 3");
+    put(0xFF0000, "South America 4");
+
+    // Europe
+    put(0x0000FF, "Europe 1");
+    put(0x0080FF, "Europe 2");
+    put(0x004080, "Europe 3");
+    put(0x000080, "Europe 4");
+    put(0x000041, "Europe 5");
+    put(0xF380FF, "Europe 6");
+    put(0x43378F, "Europe 7");
+
+    // Africa
+    put(0xFF915B, "Africa 1");
+    put(0x972900, "Africa 2");
+    put(0xAE5700, "Africa 3");
+    put(0xFF8000, "Africa 4");
+    put(0x804000, "Africa 5");
+    put(0x6F4B00, "Africa 6");
+
+    // Asia
+    put(0x562913, "Asia 1");
+    put(0x006765, "Asia 2");
+    put(0x00956D, "Asia 3");
+    put(0x008040, "Asia 4");
+    put(0x80A480, "Asia 5");
+    put(0xB3AA00, "Asia 6");
+    put(0x004000, "Asia 7");
+    put(0x627451, "Asia 8");
+    put(0x80FF00, "Asia 9");
+    put(0x008000, "Asia 10");
+    put(0x008080, "Asia 11");
+    put(0x80FF80, "Asia 12");
+
+    // Australia
+    put(0x8000FF, "Australia 1");
+    put(0xFF00FF, "Australia 2");
+    put(0x800040, "Australia 3");
+    put(0x400040, "Australia 4");
+}
