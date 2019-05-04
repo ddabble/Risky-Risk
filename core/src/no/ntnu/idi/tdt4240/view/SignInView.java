85d7eb83506e77d5b929f4caa76abec438fb4ca5
formalatist <formalatist@gmail.com>
2019-05-04 23:03:28 +0200

    The signin now properly handles canceling the signin intent

--- core/src/no/ntnu/idi/tdt4240/view/SignInView.java
+++ core/src/no/ntnu/idi/tdt4240/view/SignInView.java
@@ -14,13 +14,14 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import no.ntnu.idi.tdt4240.controller.IGPGSClient;
 import no.ntnu.idi.tdt4240.RiskyRisk;
 
+
 public class SignInView extends AbstractView {
     OrthographicCamera camera;
     Texture background;
     private Stage stage;
     private Table table;
     private IGPGSClient gpgsClient;
-
+    private boolean isSignedIn = false;
 
     public SignInView(RiskyRisk game) {
         super(game);
@@ -34,29 +35,41 @@ public class SignInView extends AbstractView {
 
         //GPGSTest
         gpgsClient = game.gpgsClient;
-
+        //set the sign in attempt handler to handle what happens if signin fails or succeeds
+        gpgsClient.setSignInAttemptHandler(new IGPGSClient.SignInAttemptHandler() {
+            @Override
+            public void onSuccess() {
+                //we can not actually switch scenes here because the return from an intent might
+                //happen at a time where libgdx doesnt expect it and shaders wont compile correctly
+                //i think???? So instead we need to set a flag that we check every frame
+                //game.setScreen(new MainMenuView(game));
+                isSignedIn = true;
+            }
+            @Override
+            public void onFailure() {
+                //failed to sign in, please try again );
+                isSignedIn = false;
+            }
+        });
     }
     @Override
     public void show() {
+        //check if we are already signed in
         if (gpgsClient.isSignedIn()) {
-            game.setScreen(new MainMenuView(this.game));
-        }
-
-
-        Button signInButton = this.createButton("Connect to google play");
-        signInButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                if (gpgsClient != null) {
-                    gpgsClient.startSignInIntent();
-                    game.setScreen(new MainMenuView(game));
+            isSignedIn = true;
+        } else { //show sign in button
+            Button signInButton = this.createButton("Connect to google play");
+            signInButton.addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if (gpgsClient != null) {
+                        gpgsClient.startSignInIntent();
+                    }
                 }
-            }
-        });
-
-
-        table.add(signInButton).pad(100);
-        table.row();
+            });
+            table.add(signInButton).pad(100);
+            table.row();
+        }
     }
 
     @Override
@@ -67,6 +80,10 @@ public class SignInView extends AbstractView {
         stage.getBatch().end();
         stage.act(delta);
         stage.draw();
+
+        if(isSignedIn) {
+            game.setScreen(new MainMenuView(this.game));
+        }
     }
 
     @Override
