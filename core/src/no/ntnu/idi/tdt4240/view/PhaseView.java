1fbb3412eaa0e3abb09d99ad72645e706fe78429
Dabble <ddabble@users.noreply.github.com>
2019-05-08 14:55:14 +0200

    Replaced UIInputProcessor with button listeners

--- core/src/no/ntnu/idi/tdt4240/view/PhaseView.java
+++ core/src/no/ntnu/idi/tdt4240/view/PhaseView.java
@@ -1,19 +1,29 @@
 package no.ntnu.idi.tdt4240.view;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import no.ntnu.idi.tdt4240.RiskyRisk;
+import no.ntnu.idi.tdt4240.controller.GameController;
 
 public class PhaseView extends AbstractView {
+    private final GameController gameController;
+
     private TextButton phaseButton;
     private Label phaseLabel;
     private Stage stage;
 
-    public PhaseView(RiskyRisk game) {
+    public PhaseView(RiskyRisk game, GameController gameController) {
         super(game);
+        this.gameController = gameController;
+    }
+
+    public Stage getStage() {
+        return stage;
     }
 
     public void updatePhase(String curPhase, String nextPhase) {
@@ -33,6 +43,12 @@ public class PhaseView extends AbstractView {
         phaseLabel.setPosition(0, 200);
         phaseButton = createButton("");
         phaseButton.setWidth(100);
+        phaseButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gameController.nextPhaseButtonClicked();
+            }
+        });
 
         stage.addActor(phaseLabel);
         stage.addActor(phaseButton);
