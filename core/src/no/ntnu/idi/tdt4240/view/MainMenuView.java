b6cd359218a00284c7224934c0af2e7f0f654a6e
Dabble <ddabble@users.noreply.github.com>
2019-05-08 04:55:03 +0200

    Removed unused methods

--- core/src/no/ntnu/idi/tdt4240/view/MainMenuView.java
+++ core/src/no/ntnu/idi/tdt4240/view/MainMenuView.java
@@ -72,29 +72,9 @@ public class MainMenuView extends AbstractView {
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
     public void hide() {
         background.dispose();
         stage.dispose();
         super.hide();
-    }
-
-    @Override
-    public void dispose() {
-
     }
 }
