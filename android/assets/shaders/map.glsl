153d0dc7ef9fd1b02ed528dc10ea87d04bb6659f
Dabble <ddabble@users.noreply.github.com>
2019-05-11 22:32:43 +0200

    Fixed `round()` not working with OpenGL ES

--- android/assets/shaders/map.glsl
+++ android/assets/shaders/map.glsl
@@ -28,10 +28,17 @@ uniform sampler2D u_texture;
 // 224 is the max number of allowed uniform vectors in a fragment shader in OpenGL ES 3.0
 uniform vec3 playerColorLookup[224]; //0xFF + 1];
 
+int roundToInt(float n)
+{
+    // OpenGl ES's `round()` does for some reason not work;
+    // it always rounds to 0 - no matter how big the number.
+    return int(n + 0.5);
+}
+
 void main()
 {
     vec4 texColor = texture2D(u_texture, v_texCoords);
     float red_hex = texColor.r * float(0xFF);
-    int territoryIndex = int(round(red_hex));
+    int territoryIndex = roundToInt(red_hex);
     gl_FragColor = vec4(playerColorLookup[territoryIndex], texColor.a);
 }
