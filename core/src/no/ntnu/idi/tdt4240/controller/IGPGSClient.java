8aacdf211b2622b3dda6134a91bf1a2e1de40f36
formalatist <formalatist@gmail.com>
2019-05-12 14:17:02 +0200

    PhasePresenter now tells BoardModel to update and send the match data if you are playing online

--- core/src/no/ntnu/idi/tdt4240/controller/IGPGSClient.java
+++ core/src/no/ntnu/idi/tdt4240/controller/IGPGSClient.java
@@ -18,7 +18,7 @@ public interface IGPGSClient {
     void onCheckGamesClicked();
 
 
-    void onDoneClicked(String data); //not sure this is needed -ø 11.05
+    void onDoneClicked(); //not sure this is needed -ø 11.05, it is, this is how we send data to the server -Ø 12.05
     void onCancelClicked();
     void onLeaveClicked();
     void onFinishClicked();
--- core/src/no/ntnu/idi/tdt4240/controller/IRiskyTurn.java
+++ core/src/no/ntnu/idi/tdt4240/controller/IRiskyTurn.java
@@ -3,7 +3,9 @@ package no.ntnu.idi.tdt4240.controller;
 import no.ntnu.idi.tdt4240.util.TerritoryMap;
 
 public interface IRiskyTurn {
+    //reads the data held in the RiskyTurn object into TerritoryMap map
     void getTerritoryMapData(TerritoryMap map);
     int getTurnCounter();
+    //updates the data held in the RiskyTurn object
     void updateData(TerritoryMap map);
 }
